218bc61dfa10c740850fe5fbe16901645a336a73
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-04-26 20:55:43 +0600

    add register nav menus

--- functions.php
+++ functions.php
@@ -6,21 +6,81 @@
 
 // Theme Title 
 add_theme_support('title-tag');
+add_theme_support('menus');
+add_theme_support('widgets');
 add_theme_support('custom-header');
+add_theme_support('post-thumbnails');
+add_theme_support('post-formats');
+add_theme_support('custom-logo');
+
+register_sidebar([
+  'name' => 'Rigth Sidebar',
+  'description'=> 'This is rigth sidebar',
+  'id' => 'rs',
+]);
+register_sidebar([
+  'name' => 'Left Sidebar',
+  'description'=> 'This is left sidebar',
+  'id' => 'ls',
+]);
+register_sidebar([
+  'name' => 'Footer Sidebar',
+  'description'=> 'This is footer bar',
+  'id' => 'fb',
+]);
+
+register_nav_menus([
+   'mm' => 'Main menu',
+   'fm' => 'Footer menu',
+]);
+
+register_post_type('Gallery', [
+  'public' => true,
+  'labels' => [
+    'name' => 'Gallery',
+    'all_items' => 'All Gallery',
+    'add_new' => 'Add New Gallery',
+    'add_new_item' => 'Add New Gallery',
+    'featured_image' => 'Gallery Image',
+    'set_featured_image' => 'Set Gallery Image',
+
+  ],
+  'supports' => [ 'thumbnail', ],
+  'menu_position' => 5,
+
+
+]);
+
+register_post_type('news', [
+  'public' => true,
+  'labels' => [
+    'name' => 'news',
+    'all_items' => 'All News',
+    'add_new' => 'Add News',
+    'add_new_item' => 'Add New News',
+
+  ],
+
+  'supports' => [ 'thumbnail',  'editor' ],
+  'menu_position' => 6,
+
+  ]);
+
+  
 
 // Theme CSS and jquery File calling
 function eva_css_js_file_calling(){
-    wp_enqueue_style('eva-style', get_stylesheet_uri());
-    wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.0.2', 'all');
-    wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
-    wp_enqueue_style('bootstrap');
-    wp_enqueue_style('custom');
-  
-  
-    //jQuery
-    wp_enqueue_script('jquery');
-    wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.0.2', 'true' );
-    wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true' );
+  wp_enqueue_style('eva-style', get_stylesheet_uri());
+  wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.0.2', 'all');
+  wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
+  wp_enqueue_style('bootstrap');
+  wp_enqueue_style('custom');
+
+
+  //jQuery
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.0.2', 'true' );
+  wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true' );
 //  wp_enqueue_style('eva-style', get_stylesheet_uri());
 //  wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.0.2');
 //  wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
@@ -31,25 +91,27 @@ function eva_css_js_file_calling(){
 //  wp_enqueue_script('jquery');
 //  wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.css', array(), '5.0.2', 'true');
 //  wp_enqueue_script('main', get_template_directory_uri().'/js/main.css', array(), '1.0.0', 'true');
-    
- 
+  
+
 }
 add_action('wp_enqueue_scripts', 'eva_css_js_file_calling');
 
 //Theme Function
 function eva_customizar_register($wp_customize){
-    $wp_customize->add_section('eva_header_area', array(
-      'title' =>__('Header Area', 'eva'),
-      'description' => 'If you interested to update your header area, you can do it here.'
-    ));
-  
-    $wp_customize->add_setting('eva_logo', array(
-      'default' => get_bloginfo('template_directory') . '/img/logo.png',
-    ));
-    $wp_customize-> add_control(new WP_Customize_Image_Control($wp_customize, 'eva_logo', array(
-        'label' => 'Logo Upload',
-        'description' => 'If you interested to change or update your logo you can do it.',
-        'setting' => 'eva_logo',
-        'section' => 'eva_header_area',
-      ) ));
+  $wp_customize->add_section('eva_header_area', array(
+    'title' =>__('Header Area', 'eva'),
+    'description' => 'If you interested to update your header area, you can do it here.'
+  ));
+
+  $wp_customize->add_setting('eva_logo', array(
+    'default' => get_bloginfo('template_directory') . '/photo.png',
+  ));
+  $wp_customize-> add_control(new WP_Customize_Image_Control($wp_customize, 'eva_logo', array(
+      'label' => 'Logo Upload',
+      'description' => 'If you interested to change or update your logo you can do it.',
+      'setting' => 'eva_logo',
+      'section' => 'eva_header_area',
+    ) ));
 }
+
+
--- index.php
+++ index.php
@@ -10,8 +10,10 @@
     <meta charset="<?php bloginfo('charset') ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
 
 <div id="header_area">
@@ -19,14 +21,20 @@
         <div class="row">
             <div class="col-md-3">
                 <a class="text-success" href=""><span >D</span> Events </a>
+                <h1><?php bloginfo('name'); ?> </h1>
+                <h1><?php bloginfo('description'); ?> </h1>
+                <h1><?php bloginfo('home'); ?> </h1>
+                <h1><?php bloginfo('url'); ?> </h1>
             </div>
             <div class="col-md-9">
+            <img src="<?php echo get_template_directory_uri(); ?>/logo.png" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/photo.png" alt="">
             </div>
         </div>
     </div>
 </div>
 
-    <?php wp_footer(); ?>
+<?php wp_footer(); ?>
     
-</body>
+    </body>
 </html>
